06f8d7cffe8f0b4b90061c1e5de9d484fdb1e08b
羽 学 <1242509682@qq.com>
2026-01-01 16:36:33 +0800

    整理代码

--- CacheData.cs
+++ CacheData.cs
@@ -4,37 +4,39 @@ using Newtonsoft.Json;
 
 namespace DeathEvent;
 
-#region 玩家数据管理方法
-public class PlayerDatas
-{
-    [JsonProperty("死亡次数", Order = 0)]
-    public int DeathCount { get; set; } = 0;
-    [JsonProperty("队伍缓存", Order = 1)]
-    public string? TeamCache { get; set; } = null;
-    [JsonProperty("切换队伍时间", Order = 2)]
-    public DateTime? SwitchTime { get; set; } = null;
-}
-#endregion
-
 // 缓存数据类
 public class CacheData
 {
+    #region 玩家缓存数据
+    public class PlayerDatas
+    {
+        [JsonProperty("死亡次数", Order = 0)]
+        public int DeathCount { get; set; } = 0;
+        [JsonProperty("队伍缓存", Order = 1)]
+        public string? TeamCache { get; set; } = null;
+        [JsonProperty("切换队伍时间", Order = 2)]
+        public DateTime? SwitchTime { get; set; } = null;
+    }
+    #endregion
+
     [JsonProperty("玩家数据")]
     public Dictionary<string, PlayerDatas> PlayerData { get; set; } = new();
     [JsonProperty("队伍死亡次数")]
     public Dictionary<string, int> TeamDeathCount { get; set; } = new();
 
+    #region 获取数据方法
     public PlayerDatas GetData(string name)
     {
-        if (!Config.DeathCache.PlayerData.TryGetValue(name, out var data))
+        if (!PlayerData.TryGetValue(name, out var data))
         {
             data = new PlayerDatas();
-            Config.DeathCache.PlayerData[name] = data;
+            PlayerData[name] = data;
         }
         return data;
     }
 
-    public int GetTeamDeath(string name) => Config.DeathCache.TeamDeathCount.TryGetValue(name, out int count) ? count : 0;
+    public int GetTeamDeath(string name) => TeamDeathCount.TryGetValue(name, out int count) ? count : 0; 
+    #endregion
 
     #region 检查切换队伍冷却
     public bool CheckSwitchCD(string name, PlayerDatas data)
